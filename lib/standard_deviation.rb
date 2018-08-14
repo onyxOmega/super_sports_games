ea394448a82adc09797d55fa1d29143869e230d1
William Fischer <william.fischer42@gmail.com>
2018-08-13 19:37:47 -0600

    Added standard devation calculator

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,10 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+def std_dev(numbers)
+  numbers[0] = numbers[0].to_f # Make sure there's a float in there
+  average = numbers.inject(:+) / numbers.length # Get the sum
+  # do the rest of the math
+  Math.sqrt(numbers.map {|e| (e - average) ** 2 }.inject(:+) / numbers.length)
+end
 
-# When you find the standard deviation, print it out
+puts std_dev(ages)
