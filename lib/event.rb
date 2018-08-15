930949ce487313766c34d0a5b12e74803a981f46
William Fischer <william.fischer42@gmail.com>
2018-08-15 15:47:31 -0600

    Completed iteration 2

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,24 @@
 class Event
-  def initialize
+  attr_reader :name, :ages, :average_age
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+  end
+
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+
+  def average_age
+    @ages.sum.to_f / @ages.length
+  end
+
+  def standard_deviation_age
+    avg = average_age
+    Math.sqrt(ages.map {|e| (e - avg) ** 2 }.sum / ages.length)
   end
 end
--- lib/runner.rb
+++
@@ -1,2 +0,0 @@
-require "./event"
-require "./standard_deviation"
