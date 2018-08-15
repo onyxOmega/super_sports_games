5fddb198643f3b5f3a0b5e972c30ac1bc6972b49
William Fischer <william.fischer42@gmail.com>
2018-08-15 15:32:55 -0600

    refactored standard deviation methods, a little

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,12 +1,12 @@
 ages = [24, 30, 18, 20, 41]
 
 def average(numbers)
-  numbers.inject(:+).to_f / numbers.length
+  numbers.sum.to_f / numbers.length
 end
 
 def std_dev(numbers)
   average = average(numbers)
-  Math.sqrt(numbers.map {|e| (e - average) ** 2 }.inject(:+) / numbers.length)
+  Math.sqrt(numbers.map {|e| (e - average) ** 2 }.sum / numbers.length)
 end
 
 puts std_dev(ages)
